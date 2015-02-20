362132f6938e1e99a4eb0da00d3af3e5af844052
monicagrau <monicagrau@gmail.com>
2015-02-20 13:00:01 +0100

    Update local_oauth.php

--- lang/ca/local_oauth.php
+++ lang/ca/local_oauth.php
@@ -16,7 +16,7 @@ $string['user_id'] = 'ID d\'usuari';
 $string['wp_url'] = 'URL del blog';
 
 $string['auth_question'] = 'Voleu autorizar l\'accés a <strong>{$a}</strong>?';
-$string['auth_question_desc'] = 'L\'aplicació está demanant accés a la informació següent del seu compte:';
+$string['auth_question_desc'] = 'L\'aplicació està demanant accés a la informació següent del seu compte:';
 $string['auth_question_login'] = 'L\'aplicació vol tenir accés a la informació de login';
 
 
@@ -25,7 +25,7 @@ $string['oauth:manageclients'] = 'Gestiona els clients del proveïdor OAuth';
 $string['client_not_exists'] = 'El client no existeix';
 $string['saveok'] = 'El client s\'ha desat correctament';
 $string['confirmdeletestr'] = 'Segur que voleu eliminar el client {$a}?';
-$string['delok'] = 'El clients\'ha eliminat correctament';
+$string['delok'] = 'El client s\'ha eliminat correctament';
 $string['client_id_existing_error'] = 'L\'identificador de client especificat ja existeix, cal seleccionar-ne un altre';
 $string['insert_error'] = 'Error creant el client';
 $string['update_error'] = 'Error actualitzant la informació del client';
@@ -33,10 +33,10 @@ $string['delete_error'] = 'Error eliminant el client';
 
 $string['scope_user_info'] = 'Informació del perfil d\'usuari/ària';
 
-$string['event_user_not_granted'] = 'Usuariària no autoritzat';
+$string['event_user_not_granted'] = 'Usuari/ària no autoritzat';
 $string['event_user_granted'] = 'Usuari/ària autoritzat';
 $string['event_user_info_request'] = 'Petició de dades d\'usuari/ària';
 $string['event_user_info_request_failed'] = 'Error en la petició de dades d\'usuari/ària';
 
-$string['client_id_help'] = 'ID de l\'aplicació per fer servir al formulari del client (Nodes o XTECBlocs) per tal de referenciar el proveïdor. Ha de ser únic. Per exemple, un ID de l\'aplicació vàlid pot ser "nodes" o "bloc1".';
+$string['client_id_help'] = 'ID de l\'aplicació per fer servir al formulari del client (XTECBlocs o Nodes) per tal de referenciar el proveïdor. Ha de ser únic. Per exemple, un ID de l\'aplicació vàlid pot ser "bloc1" o "nodes".';
 $string['redirect_uri_help'] = 'URI a on redirigir després d\'autenticar-se. Per exemple, per a XTECBlocs o Nodes, l\'URL de redirecció ha de ser semblant a: <ul><li>XTECBlocs: <i>http://blocs.xtec.cat/nomdelbloc/wp-content/plugins/wordpress-social-login/hybridauth/?hauth.done=Moodle</i></li><li>NODES: <i>http://agora.xtec.cat/nomdelcentre/wp-content/plugins/wordpress-social-login/hybridauth/?hauth.done=Moodle</i></li></ul>';
