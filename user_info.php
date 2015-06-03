d151b9135b20e4d2113681506fb1a82028e9690b
pau.ferrer-ocana <crazyserver@gmail.com>
2015-06-03 11:33:22 +0200

    Solved error handling

--- user_info.php
+++ user_info.php
@@ -8,7 +8,7 @@ require_once __DIR__.'/lib.php';
 $server = oauth_get_server();
 if (!$server->verifyResourceRequest(OAuth2\Request::createFromGlobals())) {
     $logparams = array('other' => array('cause' => 'invalid_approval'));
-    $event = \local_oauth\event\user_info_request_error::create($logparams);
+    $event = \local_oauth\event\user_info_request_failed::create($logparams);
     $event->trigger();
 
     $server->getResponse()->send();
@@ -22,7 +22,7 @@ if (isset($token['user_id']) && !empty($token['user_id'])) {
     $user = $DB->get_record('user', array('id'=>$token['user_id']), 'id,auth,username,idnumber,firstname,lastname,email,lang,country,phone1,address,description');
     if (!$user) {
         $logparams = array('other' => array('cause' => 'user_not_found'));
-        $event = \local_oauth\event\user_info_request_error::create($logparams);
+        $event = \local_oauth\event\user_info_request_failed::create($logparams);
         $event->trigger();
 
         $response->send();
@@ -33,7 +33,7 @@ if (isset($token['user_id']) && !empty($token['user_id'])) {
     $scopeRequired = 'user_info';
     if (!$server->verifyResourceRequest($request, $response, $scopeRequired)) {
         $logparams = array('relateduserid' => $user->id, 'other' => array('cause' => 'insufficient_scope'));
-        $event = \local_oauth\event\user_info_request_error::create($logparams);
+        $event = \local_oauth\event\user_info_request_failed::create($logparams);
         $event->trigger();
 
           // if the scope required is different from what the token allows, this will send a "401 insufficient_scope" error
@@ -46,7 +46,7 @@ if (isset($token['user_id']) && !empty($token['user_id'])) {
     echo json_encode($user);
 } else {
     $logparams = array('other' => array('cause' => 'invalid_token'));
-    $event = \local_oauth\event\user_info_request_error::create($logparams);
+    $event = \local_oauth\event\user_info_request_failed::create($logparams);
     $event->trigger();
 
     $server->getResponse()->send();
