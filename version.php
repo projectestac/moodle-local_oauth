efdaff58ae29ff3b96cd565abc828c9729bb6398
Pau Ferrer Ocaña <crazyserver@gmail.com>
2016-02-09 12:40:35 +0100

    Update version with component, needed for Moodle 3

--- version.php
+++ version.php
@@ -4,3 +4,4 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2014082901;   // The (date) version of this plugin
 $plugin->requires = 2011021900;   // Requires this Moodle version
+$plugin->component = 'local_oauth';
