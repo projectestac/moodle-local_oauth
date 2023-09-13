be43a4c55ee3d329e6f0285e0cf32191d4399b92
Toni Ginard <toni.ginard@gmail.com>
2023-09-13 18:48:24 +0200

    Bumped version for release

--- version.php
+++ version.php
@@ -2,6 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2016021600;   // The (date) version of this plugin
-$plugin->requires = 2011021900;   // Requires this Moodle version
+$plugin->version  = 2016021600; // The (date) version of this plugin.
+$plugin->requires = 2011021900; // Requires this Moodle version.
+$plugin->release   = 'v1.2.0';  // Human-readable version name.
 $plugin->component = 'local_oauth';
+$plugin->maturity  = MATURITY_STABLE;
